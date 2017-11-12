931c9da4afbea2316ea80a663c7d61b56cc39e59
Sandeep <sandeepsajan0@gmail.com>
2017-11-12 11:40:52 +0530

    removing unwanted changes

--- rocket/models.py
+++ rocket/models.py
@@ -1,16 +1,13 @@
 from __future__ import unicode_literals
-from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 from django.forms import ModelForm
 from django import forms
 
 
-
-
 # Create your models here.
 
-
+from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
